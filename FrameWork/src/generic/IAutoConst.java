4e23193768ab64f0b436955bb51280c1c07684a4
Welcome <Welcome@LAPTOP-QMLJP549>
2019-02-27 14:36:17 +0530

    Framework 27feb19 14:35

---
+++ FrameWork/src/generic/IAutoConst.java
@@ -0,0 +1,18 @@
+package generic;
+
+public interface IAutoConst {
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE=".\\Driver\\chromedriver.exe";
+	
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="./driver/geckodriver.exe";
+	
+	String XL_PATH="./data/Book.xlsx";
+	String IMG_PATH="./images/";
+	
+	String APP_URL="https://www.google.com";
+	
+	long ITO=10;
+	long ETO=8;
+
+}
---
+++ FrameWork/src/script/Demo.java
@@ -0,0 +1,15 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+public class Demo extends BaseTest{
+	@Test
+	public void testDemo()
+	{
+		
+		Reporter.log("Demo", true);
+	}
+}
